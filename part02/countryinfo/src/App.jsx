29de07f656aa3a85a1281266bd4107253e8f2332
Pethaf <pethaf1@gmail.com>
2023-11-15 22:52:27 +0100

    Simplified part 2

--- part02/countryinfo/src/App.jsx
+++ part02/countryinfo/src/App.jsx
@@ -4,18 +4,13 @@ import { CountriesContainer } from "./Components/CountriesContainer";
 import countryService from "./Services/Countries";
 const App = () => {
   const [allCountries, setAllCountries] = useState([])
-  const [filteredCountries, setFilteredCountries] = useState([])
   const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     countryService.getAllCountries().then(
       data => setAllCountries(data)
     )
   }, [])
-  useEffect(() => {
-    const filteredCountries = allCountries.filter(country =>
-      country.name.common.toLowerCase().includes(searchTerm.toLowerCase()))
-    setFilteredCountries(filteredCountries)
-  }, [searchTerm])
+
   const handleChange = (e) => {
     setSearchTerm(e.target.value)
   }
@@ -24,7 +19,7 @@ const App = () => {
   return (
     <>
       <Filter value={searchTerm} onChange={handleChange} />
-      <CountriesContainer countries={filteredCountries} changeSearchTerm={setSearchTerm} />
+      <CountriesContainer countries={allCountries.filter(country => country.name.common.toLowerCase().includes(searchTerm.toLowerCase()))} changeSearchTerm={setSearchTerm} />
     </>
   )
 }
