15047c2e76b6d0d6ed5e209206a0094b23dcc887
Pethaf <pethaf1@gmail.com>
2023-12-31 17:33:52 +0100

    Exercise 4.8

--- Part04/blogList/tests/bloglist_db.test.js
+++ Part04/blogList/tests/bloglist_db.test.js
@@ -7,16 +7,20 @@ const helper = require('../tests/test_helper')
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
-
 test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
-
+test('Fetches blogs from database', async () => {
+  const result = await api.get('/api/blogs')
+  expect(result.body.length).toBe(helper.initialBlogs.length)
+})
 afterAll(async () => {
   await mongoose.connection.close()
 })
