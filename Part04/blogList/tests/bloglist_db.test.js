91b08502bee355fc6c4596e499a46a1f213b4f06
Pethaf <pethaf1@gmail.com>
2023-12-31 15:51:28 +0100

    Add beforeAll and tearDown. Also first test

---
+++ Part04/blogList/tests/bloglist_db.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('../tests/test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
