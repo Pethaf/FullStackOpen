9914da8bf1bf37b20e015d48b1d25043415fa60f
Pethaf <pethaf1@gmail.com>
2024-01-01 17:33:38 +0100

    Exercise 4.9

--- Part04/blogList/tests/bloglist_db.test.js
+++ Part04/blogList/tests/bloglist_db.test.js
@@ -11,25 +11,32 @@ beforeEach(async () => {
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
 })
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-test('Fetches blogs from database', async () => {
-  const result = await api.get('/api/blogs')
-  expect(result.body.length).toBe(helper.initialBlogs.length)
-})
+describe("Structure of saved blog posts", () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  test('Fetches blogs from database', async () => {
+    const result = await api.get('/api/blogs')
+    expect(result.body.length).toBe(helper.initialBlogs.length)
+  })
 
-test('Test that fetched blog posts have a property called id ', async () => {
-  const result = await api.get('/api/blogs')
-  result.body.forEach(blog => {
-    expect(blog).toMatchSnapshot({
-      id: expect.any(String)
+  test('Test that fetched blog posts have a property called id ', async () => {
+    const result = await api.get('/api/blogs')
+    result.body.forEach(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
+  test('Test that fetched blog posts do not have a property called _id', async () => {
+    const result = await api.get('/api/blogs')
+    result.body.forEach(blog => {
+        expect(blog._id).not.toBeDefined()
     })
   })
 })
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
