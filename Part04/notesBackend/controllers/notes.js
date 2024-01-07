d5a3f4fd02cf89378f41498a87a2f0c745082634
Pethaf <pethaf1@gmail.com>
2024-01-07 18:54:15 +0100

    Add user and authentication to notes

--- Part04/notesBackend/controllers/notes.js
+++ Part04/notesBackend/controllers/notes.js
@@ -1,8 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/notes')
-
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -18,12 +26,20 @@ notesRouter.get('/:id', async (request, response) => {
 
 notesRouter.post('/', async (request, response) => {
   const body = request.body
-
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id){
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(body.userId)
+  console.log(`user: ${user}`)
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
+    user: user._id
   })
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 notesRouter.delete('/:id', async (request, response) => {
