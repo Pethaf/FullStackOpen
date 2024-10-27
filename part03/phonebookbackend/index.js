afca0de474d0201edc99fe8a5a35503dbec8c2c6
Pethaf <pethaf1@gmail.com>
2024-10-27 19:15:21 +0100

    Update index.js
    Add logging with morgan.

--- part03/phonebookbackend/index.js
+++ part03/phonebookbackend/index.js
@@ -2,6 +2,14 @@ const http=require("http");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+var morgan = require('morgan')
+app.use(morgan('combined'));
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+    console.log('POST data:', req.body);
+  }
+  next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'))
