ced8094ba6be18ee75ceb467f3d31703d90d9980
Pethaf <pethaf1@gmail.com>
2024-12-10 19:29:17 +0100

    Exercise 3.13, 3.14

--- part03/phonebookbackend/index.js
+++ part03/phonebookbackend/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
+const Person = require("./models/person");
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 var morgan = require("morgan");
+const person = require("./models/person");
 app.use(morgan("combined"));
 app.use((req, res, next) => {
   if (req.method === "POST") {
@@ -13,67 +16,35 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123457",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-53",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "12423423",
-    id: 4,
-  },
-];
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.get("/", (request, response) => {
   response.send("Backend API, please use specified urls");
 });
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => response.json(persons));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const personToDisplay = persons.find((aPerson) => aPerson.id == id);
-  if (personToDisplay) {
-    response.json(personToDisplay).status(200).end();
-  } else {
-    response.json(`Person with id ${id} not found`).status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person).status(200).end();
+    } else {
+      response.json(`Person with id ${id} not found`).status(404).end();
+    }
+  });
 });
 app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const personToDelete = persons.find((aPerson) => aPerson.id == id);
-  if (personToDelete) {
-    persons = [...persons.filter((aPerson) => aPerson.id != id)];
-    response.status(200).json(personToDelete).end();
-  }
-  response.status(404).end();
+  
 });
 app.post("/api/persons", (request, response) => {
   const personToCreate = request.body;
   if (personToCreate.name && personToCreate.number) {
-    const newPerson = {
-      ...personToCreate,
-      id: generateId(),
-    };
-    persons = [...persons, newPerson];
-    response.json(newPerson).status(201).end();
+    const newPerson = new Person({
+      name: personToCreate.name,
+      number: personToCreate.number
+    })
+    newPerson.save().then(
+      response.json(newPerson)
+    )
   } else {
     response
       .json({ error: "Include name & number for new person" })
