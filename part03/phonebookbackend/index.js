fd6026bea4a1effb242af091cd8b2f50d05c1d7a
Pethaf <pethaf1@gmail.com>
2024-12-10 19:41:14 +0100

    Exercise 3.15

--- part03/phonebookbackend/index.js
+++ part03/phonebookbackend/index.js
@@ -32,8 +32,12 @@ app.get("/api/persons/:id", (request, response) => {
     }
   });
 });
-app.delete("/api/persons/:id", (request, response) => {
-  
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id).then(
+    result => {
+      response.status(200).end()}).catch(
+        error => next(error)
+      )
 });
 app.post("/api/persons", (request, response) => {
   const personToCreate = request.body;
