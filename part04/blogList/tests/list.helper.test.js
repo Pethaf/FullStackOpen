c4cdb54bdd2c50f9f532e368c3f55782d3e694c1
Pethaf <pethaf1@gmail.com>
2024-12-22 15:06:11 +0100

    Exercise 4.4

--- part04/blogList/tests/list.helper.test.js
+++ part04/blogList/tests/list.helper.test.js
@@ -1,3 +1,4 @@
+const { listWithOneBlog,listWithTwoBlogs, listWithMultipleBlogs } = require('../assets/blogList');
 const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
@@ -10,18 +11,17 @@ test("dummy returns one", () => {
 });
 
 describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ]
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
+  })
+  test('when list has only two blogs, total likes equals sum of that', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    assert.strictEqual(result,12 )
+  })
+  test('when list has multiple blogs, equal sum of likes', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 36)
+
   })
 })
