521eaf4b0e4e26bc8147becfdc3be3948df6dd41
Pethaf <pethaf1@gmail.com>
2024-12-22 09:30:12 +0100

    Delete

--- Part04/blogListNew/tests/blogs_api.test.js
+++
@@ -1,9 +0,0 @@
-const { test, describe } = require('node:test')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const api = supertest(app)
-
-test('notes are returned as json', async () => {
-    
-})
