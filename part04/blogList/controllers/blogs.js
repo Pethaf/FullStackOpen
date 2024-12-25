1dc405f66d9fa62dd70420867d1c82233aa84a08
Pethaf <pethaf1@gmail.com>
2024-12-25 16:48:08 +0100

    Exercise 4.13

--- part04/blogList/controllers/blogs.js
+++ part04/blogList/controllers/blogs.js
@@ -13,4 +13,12 @@ blogRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
   })
   
+blogRouter.delete('/:id', async (request, response) => {
+  console.log(request.params.id)
+  if(!request.params.id){
+    response.status(404).end()
+  }
+  const deletedBlog = await Blog.findByIdAndDelete(request.params.id)
+  response.send(deletedBlog)
+})
 module.exports = blogRouter
--- part04/blogList/tests/blog_api.test.js
+++ part04/blogList/tests/blog_api.test.js
@@ -1,5 +1,5 @@
 const { listWithMultipleBlogs } = require("../assets/blogList");
-const { test, after, beforeEach } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const supertest = require("supertest");
 const mongoose = require("mongoose");
@@ -13,76 +13,108 @@ beforeEach(async () => {
   const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
 });
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("When there are blogs in the database", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("Correct number of blogs in db", async () => {
+    const response = await api.get("/api/blogs");
+    assert.strictEqual(response.body.length, listWithMultipleBlogs.length);
+  });
 });
 
-test("Correct number of blogs in db", async () => {
-  const response = await api.get("/api/blogs");
-  assert.strictEqual(response.body.length, listWithMultipleBlogs.length);
+describe("Posting blogs", () => {
+  test("Returned blogposts have field named id not _id", async () => {
+    const response = await api.get("/api/blogs").expect(200);
+    assert.ok(
+      Array.isArray(response.body),
+      response.body.forEach((item) => {
+        assert.ok(item.hasOwnProperty("id"), "Item is missing 'id' field");
+        assert.ok(!item.hasOwnProperty("_id"));
+      })
+    );
+  });
+
+  test("Valid blog post can be posted", async () => {
+    const blog = {
+      title: "Test",
+      author: "Mr.X",
+      url: "http://google.com",
+      likes: 2,
+    };
+    const savedBlog = await api.post("/api/blogs").send(blog).expect(201);
+
+    const response = await api.get("/api/blogs");
+
+    assert.deepEqual(response.body.length, listWithMultipleBlogs.length + 1);
+    for (const prop of Object.keys(blog)) {
+      assert.deepEqual(savedBlog.body[prop], blog[prop]);
+    }
+    assert.hasOwnProperty(savedBlog.body["id"]);
+  });
+  test("Blog post value likes defaults to 0", async () => {
+    const blog = {
+      author: "Mr.Y",
+      title: "Test",
+      url: "http://www.google.com",
+    };
+    const savedBlog = await api.post("/api/blogs").send(blog).expect(201);
+    assert.deepEqual(savedBlog.body.likes, 0);
+  });
+
+  test("Blog post without title won't get posted", async () => {
+    const newBlog = {
+      name: "Test",
+      url: "http://google.com",
+      likes: 4,
+    };
+    const savedBlog = await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogs = await api.get("/api/blogs");
+    assert.strictEqual(blogs.body.length, listWithMultipleBlogs.length);
+  });
+
+  test("Blog post without url won't get posted", async () => {
+    const newBlog = {
+      name: "Test",
+      author: "Mr. Y",
+    };
+    const savedBlog = await api.post("/api/blogs").send(newBlog).expect(400);
+    const blogs = await api.get("/api/blogs");
+    assert.strictEqual(blogs.body.length, listWithMultipleBlogs.length);
+  });
 });
 
-test("Returned blogposts have field named id not _id", async () => {
-  const response = await api.get("/api/blogs").expect(200);
-  assert.ok(
-    Array.isArray(response.body),
-    response.body.forEach((item) => {
-      assert.ok(item.hasOwnProperty("id"), "Item is missing 'id' field");
-      assert.ok(!item.hasOwnProperty("_id"));
-    })
-  );
+describe("Deleting blogs", async () => {
+  test("Deleting a blog post", async () => {
+    const blogToDelete = {
+      title: "DeleteMe",
+      author: "DeleteMe",
+      url: "DeleteMe",
+      likes: 3,
+    };
+    const savedBlog = await api
+      .post("/api/blogs")
+      .send(blogToDelete)
+      .expect(201);
+    const blogsBeforeDeleting = await api.get("/api/blogs");
+    await api.delete(`/api/blogs/${savedBlog.body.id}`).expect(200);
+    const blogsAfterDeleting = await api.get("/api/blogs");
+    assert.strictEqual(
+     blogsBeforeDeleting.body.length,
+     blogsAfterDeleting.body.length + 1
+    );
+    assert.ok(
+      Array.isArray(blogsAfterDeleting.body),
+      blogsAfterDeleting.body.forEach((item) => {
+        assert.ok(item._id !== savedBlog.body.id);
+      })
+    );
+  });
 });
-
-test("Valid blog post can be posted", async () => {
-  const blog = {
-    title: "Test",
-    author: "Mr.X",
-    url: "http://google.com",
-    likes: 2,
-  };
-  const savedBlog = await api.post("/api/blogs").send(blog).expect(201);
-
-  const response = await api.get("/api/blogs");
-
-  assert.deepEqual(response.body.length, listWithMultipleBlogs.length + 1);
-  for (const prop of Object.keys(blog)) {
-    assert.deepEqual(savedBlog.body[prop], blog[prop]);
-  }
-  assert.hasOwnProperty(savedBlog.body["id"]);
-});
-test("Blog post value likes defaults to 0", async () => {
-  const blog = {
-    author: "Mr.Y",
-    title: "Test",
-    url: "http://www.google.com",
-  };
-  const savedBlog = await api.post("/api/blogs").send(blog).expect(201);
-  assert.deepEqual(savedBlog.body.likes, 0);
-});
-
-test("Blog post without title won't get posted", async () => {
-  const newBlog = {
-    name: "Test",
-    url: "http://google.com",
-    likes: 4,
-  };
-  const savedBlog = await api.post("/api/blogs").send(newBlog).expect(400);
-  const blogs = await api.get("/api/blogs")
-  assert.deepEqual(blogs.body.length, listWithMultipleBlogs.length)
-});
-
-test("Blog post without url won't get posted", async () => {
-  const newBlog = {
-    name: "Test",
-    author: "Mr. Y",
-  }
-  const savedBlog = await api.post("/api/blogs").send(newBlog).expect(400);
-  const blogs = await api.get("/api/blogs")
-  assert.deepEqual(blogs.body.length, listWithMultipleBlogs.length)
-})
 
 after(async () => {
   await mongoose.connection.close();
