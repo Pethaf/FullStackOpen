3b902a5937ebb5b746b50386dd51defad5c9d099
Pethaf <pethaf1@gmail.com>
2025-01-04 17:40:33 +0100

    Exercise 4.18

--- part04/blogList/tests/login_api.test.js
+++ part04/blogList/tests/login_api.test.js
@@ -30,13 +30,28 @@ describe("Login", () => {
         }
         const loggedInUser = await api.post("/api/login").send(loginUser).expect(400)
     })
+    test("User with invalid password can't login", async () => {
+        const loginUser = {
+            username:"MrX",
+            password:"wrongpassword"
+        }
+        const loggedInUser = await api.post("/api/login").send(loginUser).expect(401)
+
+    })
+    test("User with invalid username can't login", async () => {
+        const loginUser = {
+            username: "MrZ",
+            password:"reallylongandcomplicatedpassword"
+        }
+        const loggedInUser = await api.post("/api/login").send(loginUser).expect(401)
+    })
     test("User with valid username and password can login", async () => {
         const loginUser = {
             username: "MrX",
             password: "reallylongandcomplicatedpassword"
         }
         const logedinUser = await api.post  ("/api/login").send(loginUser).expect(200)
-        assert.ok(response.body.token.startsWith("ey"), "Token should start with 'ey'");
+        assert.ok(logedinUser.body.token.startsWith("ey"), "Token should start with 'ey'");
     })
 })
 
