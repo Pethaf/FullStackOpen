5ba1ce9220055ea704823af6ff3958424113c6ac
Pethaf <pethaf1@gmail.com>
2025-01-04 17:00:41 +0100

    Tests for Exercise 4.18

---
+++ part04/blogList/tests/login_api.test.js
@@ -0,0 +1,44 @@
+const { test, after, beforeEach, describe, before } = require("node:test");
+const assert = require("node:assert");
+const supertest = require("supertest");
+const Blog = require("../models/blog");
+const User = require("../models/user");
+
+before(async () => {
+    await User.deleteMany() 
+    const testUser = {
+        name: "Mr.X",
+        username: "MrX",
+        password: "reallylongandcomplicatedpassword"
+    }
+    await api.post("/api/users/").post(testUser).expect(201)
+})
+
+describe("Login", () => {
+    test("User without password can't login", async() => {
+        const loginUser = {
+            username: "MrX",
+            password: ""
+        }
+        const loggedInUser = await api.post("/api/login").send(loginUser).expect(401)
+    })
+    test("User with without username can't login", async() => {
+        const loginUser = {
+            username: "",
+            password: "reallylongandcomplicatedpassword"
+        }
+        const loggedInUser = await api.post("/api/login").send(loginUser).expect(401)
+    })
+    test("User with valid username and password can login", async () => {
+        const loginUser = {
+            username: "MrX",
+            password: "reallylongandcomplicatedpassword"
+        }
+        const logedinUser = await api.post("/api/login").send(loginUser).expect(200)
+        assert.ok(response.body.token.startsWith("ey"), "Token should start with 'ey'");
+    })
+})
+
+after(async () => {
+  await mongoose.connection.close();
+});
